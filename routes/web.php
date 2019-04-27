469fbd5339691b4a52a28cba6baaa53389a3576f
Letícia <leticiaschmitz04@gmail.com.br>
2019-04-27 19:24:33 -0300

    web.php

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,71 @@ Route::get('/marcondes', function () {
 });
 
 
-Route::get('/produtos', 'ProdutoController@lista');
+Route::get('/produtos', 'ProdutosController@lista');
+Route::get('/produtos/formulario', "ProdutosController@formulario");
+Route::post("/produtos/gravar", "ProdutosController@gravar");
+Route::get("/produtos/deletar/{IDProduto}", "ProdutosController@deletar");
+Route::get("/produtos/alterar/formulario/{IDProduto}", "ProdutosController@formularioAlterar");
+Route::post("/produtos/alterar/{IDProduto}", "ProdutosController@alterar");
 
-Route::get('/produtos/formulario', "ProdutoController@formulario");
+Route::get('/categorias', 'CategoriasController@lista');
+Route::get('/categorias/formulario', "CategoriasController@formulario");
+Route::post("/categorias/gravar", "CategoriasController@gravar");
+Route::get("/categorias/deletar/{IDCategoria}", "CategoriasController@deletar");
+Route::get("/categorias/alterar/formulario/{IDCategoria}", "CategoriasController@formularioAlterar");
+Route::post("/categorias/alterar/{IDCategoria}", "CategoriasController@alterar");
 
-Route::post("/produtos/gravar", "ProdutoController@gravar");
+Route::get('/clientes', 'ClientesController@lista');
+Route::get('/clientes/formulario', "ClientesController@formulario");
+Route::post("/clientes/gravar", "ClientesController@gravar");
+Route::get("/clientes/deletar/{id}", "ClientesController@deletar");
+Route::get("/clientes/alterar/formulario/{id}", "ClientesController@formularioAlterar");
+Route::post("/clientes/alterar/{id}", "ClientesController@alterar");
 
-Route::get("/produtos/deletar/{id}", "ProdutoController@deletar");
+Route::get('/fornecedores', 'FornecedoresController@lista');
+Route::get('/fornecedores/formulario', "FornecedoresController@formulario");
+Route::post("/fornecedores/gravar", "FornecedoresController@gravar");
+Route::get("/fornecedores/deletar/{IDCliente}", "FornecedoresController@deletar");
+Route::get("/fornecedores/alterar/formulario/{IDCliente}", "FornecedoresController@formularioAlterar");
+Route::post("/fornecedores/alterar/{IDCliente}", "FornecedoresController@alterar");
 
-Route::get("/produtos/alterar/formulario/{id}", "ProdutoController@formularioAlterar");
-Route::post("/produtos/alterar/{id}", "ProdutoController@alterar");
+Route::get('/ordens', 'OrdensController@lista');
+Route::get('/ordens/formulario', "OrdensController@formulario");
+Route::post("/ordens/gravar", "OrdensController@gravar");
+Route::get("/ordens/deletar/{IDOrdem}", "OrdensController@deletar");
+Route::get("/ordens/alterar/formulario/{IDOrdem}", "OrdensController@formularioAlterar");
+Route::post("/ordens/alterar/{IDOrdem}", "OrdensController@alterar");
+
+Route::get('/regiao', 'RegiaoController@lista');
+Route::get('/regiao/formulario', "RegiaoController@formulario");
+Route::post("/regiao/gravar", "RegiaoController@gravar");
+Route::get("/regiao/deletar/{IDRegiao}", "RegiaoController@deletar");
+Route::get("/regiao/alterar/formulario/{IDRegiao}", "RegiaoController@formularioAlterar");
+Route::post("/regiao/alterar/{IDRegiao}", "RegiaoController@alterar");
+
+Route::get('/funcionarios', 'FuncionariosController@lista');
+Route::get('/funcionarios/formulario', "FuncionariosController@formulario");
+Route::post("/funcionarios/gravar", "FuncionariosController@gravar");
+Route::get("/funcionarios/deletar/{IDFuncionario}", "FuncionariosController@deletar");
+Route::get("/funcionarios/alterar/formulario/{IDFuncionario}", "FuncionariosController@formularioAlterar");
+Route::post("/funcionarios/alterar/{IDFuncionario}", "FuncionariosController@alterar");
+
+Route::get('/territorios', 'TerritoriosController@lista');
+Route::get('/territorios/formulario', "TerritoriosController@formulario");
+Route::post("/territorios/gravar", "TerritoriosController@gravar");
+Route::get("/territorios/deletar/{IDTerritorio}", "TerritoriosController@deletar");
+Route::get("/territorios/alterar/formulario/{IDTerritorio}", "TerritoriosController@formularioAlterar");
+Route::post("/territorios/alterar/{IDTerritorio}", "TerritoriosController@alterar");
+
+Route::get('/transportadoras', 'TransportadorasController@lista');
+Route::get('/transportadoras/formulario', "TransportadorasController@formulario");
+Route::post("/transportadoras/gravar", "TransportadorasController@gravar");
+Route::get("/transportadoras/deletar/{IDTransportadora}", "TransportadorasController@deletar");
+Route::get("/transportadoras/alterar/formulario/{IDTransportadora}", "TransportadorasController@formularioAlterar");
+Route::post("/transportadoras/alterar/{IDTransportadora}", "TransportadorasController@alterar");
+
+
+
 
 
 Auth::routes();
